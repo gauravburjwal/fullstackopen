b967a3c8e6a017c7a93e676426c75902b7657e48
Gaurav Burjwal <gauravburjwal@gmail.com>
2023-03-21 19:25:01 +0530

    part1.5 - solution

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -23,7 +23,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <p>
-      Number of exercises{" "}
+      Number of exercises
       {props.parts[0].exercises +
         props.parts[1].exercises +
         props.parts[2].exercises}
@@ -32,27 +32,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
